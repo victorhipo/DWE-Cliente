c36d6df2e18085bfb5f67f42b2a8d197e91bfdef
victor <victorvillajos@gmail.com>
2021-12-14 01:30:10 +0100

    Cookie (colores)

--- COOKIES/EJERCICIO_FINAL_COOKIES/utils/js/index.js
+++ COOKIES/EJERCICIO_FINAL_COOKIES/utils/js/index.js
@@ -6,17 +6,25 @@ let btnSSAgregar;
 let btnSSAgregarObj;
 let btnSSBorrar;
 
+let btnAgregarCookie;
+let btnBorrarCookie;
+
 window.addEventListener('load',(e)=>{
 
     instanciar();
+    colorLocalStorage();
+    colorSesionStorage();
+    
     //local
     btnLSAgregar.addEventListener('click',(e=>{
         let clave = document.querySelector('#ls-ind-clave').value;
         let valor = document.querySelector('#ls-ind-valor').value;
+        let colorLS = document.querySelector('#ls-color').value;
 
         if(clave != "" && valor != ""){
             localStorage.setItem("clave",clave);
             localStorage.setItem("valor",valor);
+            localStorage.setItem("color",colorLS);
         }
     }));
     btnLSAgregarObj.addEventListener('click',(e)=>{
@@ -34,14 +42,19 @@ window.addEventListener('load',(e)=>{
     btnSSAgregar.addEventListener('click',(e)=>{
         let clave = document.querySelector('#ss-ind-clave').value;
         let valor = document.querySelector('#ss-ind-valor').value;
+        let colorSS = document.querySelector('#ss-color').value;
+
 
         if(clave != "" && valor != ""){
             sessionStorage.setItem("clave",clave);
             sessionStorage.setItem("valor",valor);
+            sessionStorage.setItem("color",colorSS);
+
         }
 
     });
     btnSSAgregarObj.addEventListener('click',(e)=>{
+
         let nombre = document.querySelector('#ss-obj-nombre').value;
         let apellido = document.querySelector('#ss-obj-apellido').value;
         let telefono = document.querySelector('#ss-obj-tlf').value;
@@ -50,6 +63,18 @@ window.addEventListener('load',(e)=>{
         if (nombre != "" && apellido != "" && telefono != ""){
             sessionStorage.setItem("objeto", JSON.stringify(new objeto(nombre, apellido,telefono,disponivilidad)));
         }
+    });
+
+    //cookie
+    btnAgregarCookie.addEventListener('click',(e)=>{
+        let clave = document.querySelector('#clave-cookie').value;
+        let valor = document.querySelector('#valor-cookie').value;
+        let expiracion = document.querySelector('#expiracion-cookie').value;
+
+        if(clave != "" && valor != ""){
+            document.cookie = `clave= ${clave};`;
+            document.cookie = `valor= ${valor};`;
+        }        
     });
 });
 
@@ -61,4 +86,19 @@ function instanciar(){
     btnSSAgregar = document.querySelector('#ss-ind-agregar');
     btnSSAgregarObj = document.querySelector('#ss-obj-agregar');
     btnSSBorrar = document.querySelector('#ss-obj-eliminar');
+
+    btnAgregarCookie = document.querySelector('#agregar-cookie');
+    btnBorrarCookie = document.querySelector('#eliminar-cookie');
+}
+function colorLocalStorage(){
+    let color = localStorage.getItem('color');
+    document.getElementById('LocalStorage').style.background = `${color}`;
+}
+function colorSesionStorage(){
+    let color = sessionStorage.getItem('color');
+    document.getElementById('SessionStorage').style.background = `${color}`;
+}
+function colorCookie(){
+    let color = sessionStorage.getItem('color');
+    document.getElementById('LocalStorage').style.background = `${color}`;
 }
