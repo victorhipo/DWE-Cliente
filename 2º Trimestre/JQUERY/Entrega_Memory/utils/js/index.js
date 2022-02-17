afa8ed8d1b32564720c764a2ef104bda6c18aa4f
victor <victorvillajos@gmail.com>
2022-02-17 18:56:12 +0100

    putos

--- 2º Trimestre/JQUERY/Entrega_Memory/utils/js/index.js
+++ 2º Trimestre/JQUERY/Entrega_Memory/utils/js/index.js
@@ -1,8 +1,13 @@
 let cartasIzq = [];
 let cartasDer = [];
+let puntuacion = 0;
+let cIzq;
+let cDer;
 
 $(document).ready(function () {
   $("#start").click(function (e) {
+    //Si colocamos aqui el metodo pintar cartas, tenemos que encontrar
+    //la forma de bloquar el boton para no generar mas de las necesarias
     $("img").fadeIn();
   });
   $("#end").click(function (e) {
@@ -15,25 +20,27 @@ $(document).ready(function () {
   $("#Izq img").click(function (e) {
     $(e.currentTarget).fadeOut();
     $(e.currentTarget).attr("src", cartasIzq[e.currentTarget.alt].img);
+    cIzq = e.currentTarget.alt;
     $(e.currentTarget).fadeIn();
-  });
+
+    });
   $("#Der img").click(function (e) {
     $(e.currentTarget).fadeOut();
     $(e.currentTarget).attr("src", cartasDer[e.currentTarget.alt].img);
+    cDer = e.currentTarget.alt;
     $(e.currentTarget).fadeIn();
   });
+
+  calcularPuntuacion();
 });
+
 
 function cargarCartas() {
   for (let i = 0; i < 18; i++) {
     cartasDer.push(new Carta(`utils/img/${i}.png`, i));
     cartasIzq.push(new Carta(`utils/img/${i}.png`, i));
   }
-  this.cartasDer = _.shuffle(cartasDer);
-  this.cartasIzq = _.shuffle(cartasIzq);
-
-  console.log(cartasDer);
-  console.log(cartasIzq);
+  //Metodo cartas aleatorias :
 }
 
 function pintarCartas(lado) {
@@ -46,7 +53,10 @@ function pintarCartas(lado) {
 
   $.each(array, function (i, element) {
     $(`#${lado}`).append(
-      `<img src='${element.fondo}' style="width: 15%;" alt='${element.valor}'">`
+      `<img src='${element.fondo}' style="width: 15%; margin: 4px;" alt='${element.valor}'">`
     );
   });
+}
+function calcularPuntuacion() {
+  $('#puntos').append(`<p>${puntuacion}</p>`);
 }
